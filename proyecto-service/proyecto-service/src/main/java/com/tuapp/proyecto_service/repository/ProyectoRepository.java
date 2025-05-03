0ef5e2a7a0f5d7ef5f014a3abc248638d0b98a99
14Katherine <aksanchez@unicauca.edu.co>
2025-05-02 14:49:25 -1200

    Update ProyectoRepository.java

--- proyecto-service/proyecto-service/src/main/java/com/tuapp/proyecto_service/repository/ProyectoRepository.java
+++ proyecto-service/proyecto-service/src/main/java/com/tuapp/proyecto_service/repository/ProyectoRepository.java
@@ -8,11 +8,11 @@ import java.util.List;
 
 public interface ProyectoRepository extends JpaRepository<Proyecto, Long> {
 
-    // 🔎 Buscar proyectos por empresa
+    //  Buscar proyectos por empresa
     List<Proyecto> findByEmpresaId(Long empresaId);
 
 
-    // 🔎 Buscar proyectos por estado (APROBADO, PENDIENTE, RECHAZADO, etc.)
+    // Buscar proyectos por estado (APROBADO, PENDIENTE, RECHAZADO, etc.)
     List<Proyecto> findByEstado(String estado);
     @Query("SELECT p FROM Proyecto p WHERE p.estado = 'APROBADO'")
     List<Proyecto> findProyectosAprobados();
